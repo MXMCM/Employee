b2845fca18fdb7790978eeeb78722ce636ec5acc
n57r <grntboi90@gmail.com>
2022-01-14 07:58:27 -0800

    Third

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const EmployeeController = require('../Controllers/EmployeeController');
 
 /* GET home page. */
 router.get('/', EmployeeController.displayEmployees);
-
+router.get('/add', EmployeeController.renderAddEmployeeForm);
+router.get('/add', EmployeeController.addEmployee);
+router.get('/update/:id', EmployeeController.renderUpdateForm);
+router.get('/update/:id', EmployeeController.updateEmployee);
 module.exports = router;
